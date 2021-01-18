cfd329f3846086ccc31f1805034914c700e200f3
Jose Rodriguez <jarodriguez045@gmail.com>
2021-01-18 17:04:43 -0500

    added media files url and root

--- app/project/settings.py
+++ app/project/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Media files
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
